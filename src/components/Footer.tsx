6dbe382501bb9215f5cac6b0b40ef9bdc42d050b
Eric Hendrickson <hendrickson02@gmail.com>
2023-04-28 12:43:11 -0700

    * Reformatted according to prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,24 @@
 export default function Footer() {
-  return (
-    <footer className="flex h-24 w-full bg-zinc-900">
-      <div className="mx-auto flex w-full max-w-screen-2xl flex-col items-start justify-between p-4">
-        <p className="w-full whitespace-nowrap text-sm font-medium dark:text-white">&copy; Eric Hendrickson 2023</p>
-        <p className="w-full whitespace-nowrap pb-4 text-sm font-medium dark:text-white">
-          This page was created using the Next.js framework. Want to learn how to build one just like this? Click{' '}
-          <a className="font-bold text-blue-400 underline" target="_blank" href="https://nextjs.org/" rel="noreferrer">
-            here
-          </a>
-          .
-        </p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="flex h-24 w-full bg-zinc-900">
+            <div className="mx-auto flex w-full max-w-screen-2xl flex-col items-start justify-between p-4">
+                <p className="w-full whitespace-nowrap text-sm font-medium dark:text-white">
+                    &copy; Eric Hendrickson 2023
+                </p>
+                <p className="w-full whitespace-nowrap pb-4 text-sm font-medium dark:text-white">
+                    This page was created using the Next.js framework. Want to learn how to build one just like this?
+                    Click{' '}
+                    <a
+                        className="font-bold text-blue-400 underline"
+                        target="_blank"
+                        href="https://nextjs.org/"
+                        rel="noreferrer"
+                    >
+                        here
+                    </a>
+                    .
+                </p>
+            </div>
+        </footer>
+    );
 }
--- src/components/HamburgerLink.tsx
+++ src/components/HamburgerLink.tsx
@@ -1,12 +1,12 @@
 import { ILink } from '@/models/ILink';
 
 export default function HamburgerLink({ label, url }: ILink) {
-  return (
-    <a
-      href={url}
-      className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-    >
-      {label}
-    </a>
-  );
+    return (
+        <a
+            href={url}
+            className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+        >
+            {label}
+        </a>
+    );
 }
