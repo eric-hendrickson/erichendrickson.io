1129b224e259fe8e4d829d8aa5dc306ed87332c7
Eric Hendrickson <hendrickson02@gmail.com>
2023-05-02 12:48:02 -0700

    * Added Material UI (mui) * Styled the contact form

--- src/components/homepage/Contact.tsx
+++ src/components/homepage/Contact.tsx
@@ -1,53 +1,57 @@
+'use client';
+
 import HomepageSection from '@/components/homepage/HomepageSection';
+import { Box, TextField, Button } from '@mui/material';
 
 export default function Contact() {
     return (
         <HomepageSection id="contact" heading="Contact" sectionBackgroundColor="bg-blue-300">
-            <div className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-center">
-                <p className="py-4 text-xl">
-                    Want to work together? Know of any exciting opportunities I may be interested in? Maybe you just
-                    want to say hello. No problem! Feel free to send me a message using the form below!
-                </p>
-                <div>
-                    <form>
-                        <input
-                            id="name"
-                            type="text"
-                            // value={inputs.name}
-                            // onChange={handleChange}
-                            // className={styles.inputField}
-                            placeholder="Name"
-                            required
-                        />
-                        <input
-                            id="email"
-                            type="email"
-                            // value={inputs.email}
-                            // onChange={handleChange}
-                            // className={styles.inputField}
-                            placeholder="Email"
-                            required
-                        />
-                        <textarea
-                            id="message"
-                            // type="text"
-                            // value={inputs.message}
-                            // onChange={handleChange}
-                            // className={styles.inputField}
-                            placeholder="Message"
-                            // rows="5"
-                            required
-                        />
-                        <input type="submit" />
-                        {/* {form.state === 'loading' ? (
-                            <div>Sending....</div>
-                        ) : form.state === 'error' ? (
-                            <div>{form.message}</div>
-                        ) : (
-                            form.state === 'success' && <div>Sent successfully</div>
-                        )} */}
-                    </form>
-                </div>
+            <div className="mx-auto flex max-w-screen-2xl flex-col flex-wrap items-center justify-center">
+                <p className="py-2 text-xl">Want to work together?</p>
+                <p className="py-2 text-xl">Know of any exciting opportunities I may be interested in?</p>
+                <p className="py-2 text-xl">Maybe you just want to say hello.</p>
+                <p className="my-2 py-2 text-xl">No problem! Feel free to send me a message using the form below!</p>
+                <Box
+                    component="form"
+                    onSubmit={() => console.log('submitted')}
+                    noValidate
+                    className="rounded bg-gradient-to-br from-yellow-50 to-cyan-200 px-3"
+                >
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="name"
+                        label="Name"
+                        type="text"
+                        id="name"
+                        autoComplete="name"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        multiline
+                        minRows={4}
+                        id="message"
+                        label="Message"
+                        name="message"
+                        autoFocus
+                    />
+                    <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} className="bg-green-700">
+                        Sign In
+                    </Button>
+                </Box>
             </div>
         </HomepageSection>
     );
