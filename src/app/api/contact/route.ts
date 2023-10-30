19cc18fc255e61292532a93aac0626adf0c63dfa
Eric Hendrickson <hendrickson02@gmail.com>
2023-10-30 01:36:27 +0000

    * Trying to add cors headers

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -60,6 +60,11 @@ export async function POST(request: NextRequest) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT',
+                'Access-Control-Allow-Headers':
+                    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
             },
             body: JSON.stringify({ name, email, message }),
         });
