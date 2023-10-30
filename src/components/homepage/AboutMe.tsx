81b05e165d0539af029c44bbaee12736f6c754b9
Eric Hendrickson <hendrickson02@gmail.com>
2023-10-30 06:08:46 +0000

    * Removed awkward console logs and added toasts

--- src/components/homepage/AboutMe.tsx
+++ src/components/homepage/AboutMe.tsx
@@ -17,8 +17,7 @@ export default function AboutMe() {
                 </p>
                 <p className="py-4 text-xl">
                     Feel free to connect with me or follow me on the following socials, or view or download my resume.
-                    If you want to contact me, please feel free to use the form at the bottom of this page (Work in
-                    Progress).
+                    If you want to contact me, please feel free to use the form at the bottom of this page.
                 </p>
                 {/* <div className="flex w-full flex-row items-center justify-center py-4">SOCIALS HERE</div>
                 <a className="py-4 text-xl">VIEW/DOWNLOAD MY LATEST RESUME</a> */}
